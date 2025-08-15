6c81eb5c9872c4af85e74c2a1da22f1d7b543025
Josué <12565874+jjwakn@users.noreply.github.com>
2025-08-14 20:55:06 -0600

    Fixed lint and prettier

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -17,6 +17,10 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    parserOptions: {
+      tsconfigRootDir: __dirname,
+      project: './tsconfig.json',
+    },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
@@ -25,4 +29,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+);
